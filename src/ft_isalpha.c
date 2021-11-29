5089e883fc831907fac95b026fc3a5eff0501986
Carlos Sierra Doncel <C.SierraDoncel@gmail.com>
2021-11-29 11:46:34 +0100

    ft_isalpha first commit

---
+++ src/ft_isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+
+
+int ft_isalpha(int c)
+{
+	return((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+int main()
+{
+	printf("%d\n", ft_isalpha('3'));
+
+	return 0;
+}
