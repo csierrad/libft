e67848f5f06c23e21766c5a06af28870ede39362
Carlos Sierra Doncel <C.SierraDoncel@gmail.com>
2021-11-30 10:14:17 +0100

    ft_isdigit first commit

---
+++ src/ft_isdigit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+
+int ft_isdigit(int c)
+{
+	return(c >= '0' && c <= '9');
+}
+
+
+/*
+int main()
+{
+	printf("%d\n", ft_isdigit('3'));
+
+	return 0;
+}
+*/
